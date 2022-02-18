c917b138c59bf4b564b81bd1a5f10c0fd8709648
oscargarciaporras <osligarpo92@gmail.com>
2022-02-17 21:46:43 -0500

    funciones 17_02_2022

---
+++ main.js
@@ -0,0 +1,41 @@
+//Funcion por DECLARACION
+const saludar = function saludarRobot(num) {
+    let nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
+    alert(`Hola usuario ${nombre} me alegra conocerte`);
+};
+saludar(1);
+saludar(2);
+saludar(3);
+saludar(4);
+saludar(5);
+
+
+
+
+
+
+
+
+
+
+// saludarRobot(1);
+// saludarRobot(2);
+// //Funcion por EXPRESION
+// function saludarRobot(num) {
+//     let nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
+//     alert(`Hola usuario ${nombre} me alegra conocerte`);
+// }
+// saludarRobot(3);
+// saludarRobot(4);
+// saludarRobot(5);
+
+
+
+
+
+
+
+// let num = 1, nombre = null;
+
+// nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
+// alert(`Hola usuario ${nombre} me alegra conocerte`);
